1ecb11900e892f5abbd9fb6baf53ef0e91e56b60
Adrian Schlatter <loepfe-schl@users.noreply.github.com>
2023-07-21 11:55:42 +0200

    basic cli tool
    provides:
    
    * cobs decoding
    * unpacking (convert to data frames with .log_id, .timestamp,
      .samplecount, .payload)
    * unhexing: Convert binary data (represented as hex string) to numbers

--- src/rawtools/cli.py
+++ src/rawtools/cli.py
@@ -4,7 +4,35 @@
 """
 
 from plumbum import cli
-import pkg_resources  # part of setuptools
+import pkg_resources
+from sys import stdout, stdin, exit
+import os
+import numpy as np
+import json
+from rawtools import decobs, unpack
+
+
+class NumpyJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+
+        return super(NumpyJSONEncoder, self).default(obj)
+
+
+def write_to_stdout(items):
+
+    try:
+        for item in items:
+            stdout.write(item + os.linesep)
+    except BrokenPipeError:
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, stdout.fileno())
+        exit(1)
 
 
 class RawTool(cli.Application):
@@ -15,16 +43,80 @@ class RawTool(cli.Application):
     PROGNAME = 'rt'
     VERSION = pkg_resources.require('rawtools')[0].version
 
-    def main(self, *args):
+    def main(self):
         pass
 
 
-@RawTool.subcommand('decode')
+@RawTool.subcommand('decobs')
 class RTdecode(cli.Application):
-    "cobs decode package stream"
+    """COBS-decode binary packets from named file."""
 
-    def main(self, *args):
-        print('Not implemented, yet. Stay tuned!', *args)
+    @cli.positional(cli.ExistingFile)
+    def main(self, filename=None):
+        if filename is None:
+            source = stdin.buffer  # binary mode
+        else:
+            source = open(filename, 'rb')
+
+        with source as f:
+            msgs = decobs(f.read())
+            write_to_stdout([msg.hex() for msg in msgs])
+
+
+@RawTool.subcommand('unpack')
+class RTunpack(cli.Application):
+    """
+    Convert raw messages to frames.
+
+    Frames having .logid, .timestamp, .samplecount, and .payload.
+    """
+
+    @cli.positional(cli.ExistingFile)
+    def main(self, filename=None):
+        if filename is None:
+            source = stdin  # text mode
+        else:
+            source = open(filename, 'r')
+
+        with source as f:
+            hexlines = f.readlines()
+            msgs = [bytes.fromhex(line) for line in hexlines]
+            frames = unpack(msgs)
+            write_to_stdout([frame.as_json() for frame in frames])
+
+
+@RawTool.subcommand('dehex')
+class RTdehex(cli.Application):
+    """Convert hex string to numbers of a certain type."""
+
+    strip = cli.Flag('strip', help='Strip quotes at ends of lines')
+    concat = cli.Flag('concat', help='Concatenate output into single list')
+
+    @cli.positional(str, cli.ExistingFile)
+    def main(self, datatype, filename=None):
+        if filename is None:
+            source = stdin  # text mode
+        else:
+            source = open(filename, 'r')
+
+        with source as f:
+            hexlines = f.readlines()
+            stripchars = '"\'\n' if self.strip else '\n'
+            binaries = [bytes.fromhex(line.strip(stripchars))
+                        for line in hexlines]
+            dt = np.dtype(datatype)
+            dt.newbyteorder('<')
+            arrays = [np.frombuffer(binary, dtype=dt) for binary in binaries]
+
+            if self.concat:
+                if len(arrays) > 0:
+                    array = np.concatenate(arrays)
+                else:
+                    array = np.empty((0, ), dtype=dt)
+                write_to_stdout([json.dumps(array, cls=NumpyJSONEncoder)])
+            else:
+                write_to_stdout([json.dumps(array, cls=NumpyJSONEncoder)
+                                 for array in arrays])
 
 
 def main():
